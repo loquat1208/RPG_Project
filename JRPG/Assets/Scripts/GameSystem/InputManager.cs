2039638dc66edd8e8e074763906ca5c35494ba01
123 <student@MacBookPro-14.local>
2017-07-05 11:53:57 +0900

    JRPG START

---
+++ JRPG/Assets/Scripts/GameSystem/InputManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+	/*
+	 WASD_MOUSE : Change position with WASD, direction with Mouse;
+	 */
+	public enum INPUT_METHOD
+	{
+		WASD_MOUSE,
+	}
+
+	[SerializeField] private INPUT_METHOD input_method;
+
+	private Vector3 m_pos;
+	private Vector3 m_dir;
+
+	public Vector3 GetPosInput { get { return m_pos; } }
+	public Vector3 GetDirInput { get { return m_dir; } }
+
+	void Update () {
+		switch (input_method) {
+		case INPUT_METHOD.WASD_MOUSE:
+			UpdateWASDMouse ();
+			break;
+		}
+	}
+
+	void UpdateWASDMouse( ) {
+		m_pos.x = Input.GetAxis ("Horizontal");
+		m_pos.z = Input.GetAxis ("Vertical");
+	}
+}
---
+++ JRPG/Assets/Scripts/GameSystem/SingleCharaterRPGSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleCharaterRPGSystem : MonoBehaviour {
+	[SerializeField] private GameObject main_character;
+
+	private InputManager m_input_manager;
+
+	void Start () {
+		m_input_manager = GetComponent<InputManager> ();
+		if (!m_input_manager) {
+			Debug.LogError ("'GameSystem' need 'InputManager' commponent. Plaese put 'InputManager' script in 'GameSystem' gameobject");
+		}
+	}
+
+	void Update () {
+		
+	}
+
+	void CharacterMove( ) {
+		
+	}
+}
